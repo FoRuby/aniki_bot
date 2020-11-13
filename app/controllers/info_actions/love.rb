16f3762117c54ad038a533f04d8391e3da728bdd
FoRuby <aggabitov@gmail.com>
2020-11-13 15:57:24 +0300

    love fix

--- app/controllers/info_actions/love.rb
+++ app/controllers/info_actions/love.rb
@@ -1,6 +1,15 @@
 module InfoActions::Love
-  def love!(*)
-    reply_with :message, text: love_quotes.sample.to_s
+  def love!(*args)
+    args.present? ? send_love_to(args) : reply_with(:message, text: love_quotes.sample.to_s)
+  end
+
+  def send_love_to(users)
+    users = users.map { |usertag| usertag[1..-1] }
+                 .map { |username| User.find_by_username(username) }
+                 .compact
+    users.each do |user|
+      bot.send_message chat_id: user.chat_id, text: "#{current_user.usertag} loves you!"
+    end
   end
 
   def love_quotes
