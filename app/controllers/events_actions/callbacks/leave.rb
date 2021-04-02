5b1a6be1b735bbfcd619d220d5875f3ee42093b3
R1zrei <aggabitov@gmail.com>
2021-04-02 03:34:14 +0300

    Event::Leave

--- app/controllers/events_actions/callbacks/leave.rb
+++ app/controllers/events_actions/callbacks/leave.rb
@@ -6,11 +6,9 @@ module EventsActions
           current_user: current_user, params: { event_id: event_id, user_id: current_user.id }
         )
         if operation.success?
-          response = Render::Operation::ShowEvent.call(event: operation[:model].event)[:response]
-          edit_message :text, response
-          answer_callback_query(t('telegram_webhooks.leave_callback_query.success'), show_alert: true)
+          Event::Operation::Response::Leave::Success.call(payload: payload, current_user: current_user, operation: operation)
         else
-          answer_callback_query(render_errors(operation), show_alert: true)
+          Event::Operation::Response::Leave::Failure.call(payload: payload, current_user: current_user, operation: operation)
         end
       end
     end
